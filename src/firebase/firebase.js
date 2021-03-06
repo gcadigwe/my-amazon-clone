17072f43ba8b9131219c85ef6e3ca053877800ec
GCAdigwe <gcadigwe@gmail.com>
2021-03-06 11:40:54 +0000

    added and styled header

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARSGyVpwr0A4mxXBi_Xhb4xywNbNt_dPQ",
+  authDomain: "fir-e517c.firebaseapp.com",
+  projectId: "fir-e517c",
+  storageBucket: "fir-e517c.appspot.com",
+  messagingSenderId: "222691559671",
+  appId: "1:222691559671:web:18cc66bfc58a5ccfefebf2",
+  measurementId: "G-K2PH0P2W1R",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
