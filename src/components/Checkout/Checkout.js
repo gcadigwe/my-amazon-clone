22ef9a14330564a0a2578cccb875fe902aa4a380
GCAdigwe <gcadigwe@gmail.com>
2021-03-08 14:34:43 +0000

    finished styling the checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Subtotal from "../Subtotal/Subtotal";
 import "./Checkout.css";
 
 function Checkout() {
   return (
     <div className="checkout">
-      <div className="checout__left">
+      <div className="checkout__left">
         <img
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
@@ -15,7 +16,7 @@ function Checkout() {
         </div>
       </div>
       <div className="checout__right">
-        <h2>The subtotal will go here</h2>
+        <Subtotal />
       </div>
     </div>
   );
