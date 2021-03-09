c395f57b50552c7b38c60713b3043f1a0619eb92
GCAdigwe <gcadigwe@gmail.com>
2021-03-09 11:16:12 +0000

    Login form frontend done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import { Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="app">
       {/* header */}
-      <Header />
+
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/checkout" exact component={Checkout} />
+        <Route path="/" exact>
+          <Header />
+          <Home />
+        </Route>
+        <Route path="/checkout" exact>
+          <Header />
+          <Checkout />
+        </Route>
+        <Route path="/login" exact component={Login} />
       </Switch>
     </div>
   );
